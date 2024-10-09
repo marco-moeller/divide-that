4b759cdc80386c8d6b4c4f3c67e408509d32d0fb
Marco Moeller <maaakooo@gmx.de>
2024-10-09 11:35:32 +0200

    pwa plugin
    added the vite pwa plugin for phone desktop support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "SplitWiser",
+        short_name: "SplitWiser",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-72.png",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "icon-128.png",
+            sizes: "128x128",
+            type: "image/png"
+          },
+          {
+            src: "icon-144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
+    react()
+  ]
+});

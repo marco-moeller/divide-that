09fa4ee4e8ff4b8cb3c073142dd115d26b5cfa2a
Marco Moeller <maaakooo@gmx.de>
2025-01-04 13:18:40 +0100

    bug: could click on deleted group/expense activity

--- src/components/activities/ActivityComponent.jsx
+++ src/components/activities/ActivityComponent.jsx
@@ -2,22 +2,30 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { convertToMonthDayTime } from "../../utility/date";
 import { formatActivity } from "../../utility/interfaces";
+import useExpense from "../../hooks/useExpense";
+import useGroup from "../../hooks/useGroup";
 
 function ActivityComponent({ activity }) {
   const { user } = useAuth();
+  const { expense } = useExpense(activity?.expenseID);
+  const { group } = useGroup(activity.groupID);
 
   if (!user) {
     return <></>;
   }
 
   const getActivityLink = () => {
-    if (activity?.expenseID) return `../expense/${activity?.expenseID}`;
-    if (activity?.groupID) return `../group/${activity?.groupID}`;
+    if (activity?.expenseID && expense)
+      return `../expense/${activity?.expenseID}`;
+    if (activity?.groupID && group) return `../group/${activity?.groupID}`;
     else return "";
   };
 
   return (
-    <NavLink to={getActivityLink()}>
+    <NavLink
+      to={getActivityLink()}
+      className={getActivityLink() === "" ? "no-pointer" : ""}
+    >
       <div className="activity">
         <h3>{formatActivity(activity, user.id)}</h3>
         <h3 className="date">

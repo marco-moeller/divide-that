9fc2364a0cfa737f8d53ed9d7ea649dd2b74b428
Marco Moeller <maaakooo@gmx.de>
2025-01-06 12:16:36 +0100

    other: removed one ad block

--- src/components/ads/MonetagAd.jsx
+++ src/components/ads/MonetagAd.jsx
@@ -19,24 +19,6 @@ function MonetagAd() {
     };
   }, []);
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://vemtoutcheeg.com/400/8747483";
-    script.async = true;
-
-    try {
-      (document.body || document.documentElement).appendChild(script);
-    } catch (e) {
-      console.error("Error appending script:", e);
-    }
-
-    return () => {
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
-    };
-  }, []);
-
   return null; // No visible UI for this ad
 }
 
